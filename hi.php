308ad826b8aa35f3de1530935eb1b9021fdc2e31
MichaelRay <michaelray@vip.qq.com>
2020-04-07 22:54:13 +0800

    first commit

---
+++ hi.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 雷霆科技 <michaelray@vip.qq.com> <http://www.thunderfuture.com>
+ * Date: 2020/4/7
+ * Time: 22:40
+ */
+require_once './vendor/autoload.php';
+
+use Tools\Tools;
+
+Tools::hi();
