e1c023439950a5b4b6d7afca3f45711f8659f037
MichaelRay <michaelray@vip.qq.com>
2020-04-09 18:31:26 +0800

    修复BUG

--- src/config/socket_log.php
+++ src/config/socket_log.php
@@ -7,8 +7,14 @@
  */
 
 return [
-	//强制输出到client_id
-	'force_client_id'  => '',
+	'enable'              => true, //是否记录日志的开关
+	'host'                => 'localhost',
+	//是否显示利于优化的参数，如果允许时间，消耗内存等
+	'optimize'            => true,
+	'show_included_files' => false,
+	'error_handler'       => false,
+	//日志强制记录到配置的client_id
+	'force_client_id'     => '',
 	//限制允许读取日志的client_id
-	'allow_client_ids' => [],
+	'allow_client_ids'    => []
 ];
