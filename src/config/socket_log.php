ff6818d6f08d41af7025739d4da0ddf9f88dee55
MichaelRay <michaelray@vip.qq.com>
2020-04-09 14:42:27 +0800

    修复BUG

---
+++ src/config/socket_log.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 雷霆科技 <michaelray@vip.qq.com> <http://www.thunderfuture.com>
+ * Date: 2020/4/9
+ * Time: 13:20
+ */
+
+return [
+	//强制输出到client_id
+	'force_client_id'  => '',
+	//限制允许读取日志的client_id
+	'allow_client_ids' => [],
+];
